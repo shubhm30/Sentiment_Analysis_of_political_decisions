81ebf6e12fa9e7a5c35760cbb58dfa5798af0533
Shubham Chaudhary <shubhamchaudhary30@yahoo.com>
2022-06-08 02:37:05 +0530

    secret_keys_managed

---
+++ config.py
@@ -0,0 +1,6 @@
+#saving twitter access secret keys
+
+consumer_key = "v5mm7C16zrHLLPipFStCjqhBA"
+consumer_secret = "mrbbQiRb2CkT4tl0xqYvYZhPxELGQ12W52UotVwq3YBAJ3yMVq"
+access_key = "53633496-blZ8jZJXQGby9HgqHIqaPRVvIIePFUe8BxxEKrZDI"
+access_secret = "OepRbyXMiw5nNH7wNtnTWRPZ9Dv9GCVFVJmY2M9HDyQQA"
--- extract.py
+++ extract.py
@@ -1,10 +1,6 @@
 import tweepy
 import json
-
-consumer_key = "v5mm7C16zrHLLPipFStCjqhBA"
-consumer_secret = "mrbbQiRb2CkT4tl0xqYvYZhPxELGQ12W52UotVwq3YBAJ3yMVq"
-access_key = "53633496-blZ8jZJXQGby9HgqHIqaPRVvIIePFUe8BxxEKrZDI"
-access_secret = "OepRbyXMiw5nNH7wNtnTWRPZ9Dv9GCVFVJmY2M9HDyQQA"
+import config
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
